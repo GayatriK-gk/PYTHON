14f69971574aff74ba58f527408f85d81bfb8c4d
GayatrK-gk <gayatrik6566@gmail.com>
2026-02-17 23:03:30 +0530

    input

---
+++ Tasks/input.py
@@ -0,0 +1,10 @@
+"""input("Enter Your Name:")
+name=input("enter your name")
+print("welcome",name)"""
+
+name=input("enter your name")
+age=input("enter your age")
+marks=input("enter your marks")
+print("Name:",name)
+print("age:",age)
+print("marks:",marks)
--- Tasks/task1.py
+++ Tasks/task1.py
@@ -1,1 +1,13 @@
 print("Hello")
+print(24)
+print(2+2)
+
+name="Gayatri"
+age=22
+Dept="cs"
+print(name)
+print(age)
+print(Dept)
+
+print(type(name))
+print(type(age))
