86ff5fcf1e958956a1881cff06f12e6141c0617d
GayatrK-gk <gayatrik6566@gmail.com>
2026-02-25 23:25:30 +0530

    InputSchool

---
+++ Tasks/input4.py
@@ -0,0 +1,18 @@
+print("School Information")
+
+name=input("enter your name:")
+age=int(input("enter your age:"))
+div=input("enter your division:")
+sname=input("enter your school name:")
+marks=int(input("enter your marks:"))
+
+print("School Name:",sname)
+print("-------------------")
+print("Name Of The Student:",name)
+print("Age of the Student:",age)
+print("Divisioin:",div)
+print("marks:",marks)
+
+print("---------------------")
+
+print(name,"is very Politie Student ")
